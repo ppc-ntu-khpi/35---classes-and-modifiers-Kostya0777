8fc0f1ec1b961e7dac7573a6156638f45a377023
Kostya0777 <72386928+Kostya0777@users.noreply.github.com>
2020-11-03 14:34:53 +0200

    Add files via upload

---
+++ CustomerTest.java
@@ -0,0 +1,11 @@
+public class CustomerTest {
+
+	public static void main(String[] args) {
+		Сustomer c = new Сustomer();
+    c.setID(17);
+    c.setNew(false);
+    c.setTotal(554.55);
+    c.displayCustomerInfo();
+	}
+
+}
